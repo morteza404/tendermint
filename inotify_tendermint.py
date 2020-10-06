036733043a1f31d83762dc2623140ac7180932b6
Morteza Shahbazi <mortezash404@gmail.com>
2020-10-06 13:25:48 +0330

    edit inotify_tendermint.py

--- inotify_tendermint.py
+++ inotify_tendermint.py
@@ -4,7 +4,8 @@ import base58
 import base64
 import json
 
-PATH = "/home/shahbazi/Desktop/notify_dir"
+PATH = "/home/shahbazi/Desktop/rings/"
+RING_LIST = ["object.builder", "container.builder", "account.builder", "object.ring.gz", "container.ring.gz", "account.ring.gz",]
 
 def notify(path):
     flag = False
@@ -13,21 +14,24 @@ def notify(path):
     try:
         for event in i.event_gen(yield_nones=False):
             (_, type_names, path, filename) = event
-            # print (type_names)
+            #print (type_names)
+            # print (type(filename))
             if "IN_MODIFY" in type_names:
                 flag = True
-            if flag and "IN_CLOSE_WRITE" in type_names :
-                flag = False
+            if (flag and "IN_CLOSE_WRITE" in type_names)  or ("IN_MOVED_TO" in type_names) :
                 # print(f"PATH=[{path}] FILENAME=[{filename}] EVENT_TYPES={type_names}") 
                 # print("salam :)")
-                send_transaction("account.builder","account.builder")               
+                send_transaction(filename,filename) 
+            if (flag and "IN_CLOSE_WRITE" in type_names):
+                flag = False
+                         
     except KeyboardInterrupt:
         pass
     finally:
         i.remove_watch(path)
 
 def send_transaction(trx_key, ring_file):
-    with open(f"/home/shahbazi/Desktop/rings/{ring_file}","rb") as file:
+    with open(PATH + ring_file,"rb") as file:    
         s = file.read()
         # print(s)
     trx_value = base58.b58encode(s)
@@ -43,9 +47,23 @@ def get_transaction_value(trx_key, new_ring_file):
     b64_list = str(b64_value).split('b"b')[1].replace('"','').replace('\'','')
     b64_bytes = bytes(b64_list, 'utf-8') 
     print(base58.b58decode(b64_bytes))
-    with open(f"/home/shahbazi/Desktop/rings/{new_ring_file}","wb") as file:
+    with open(PATH + f"{new_ring_file}","wb") as file:
         file.write(base58.b58decode(b64_bytes))
 
+def get_trx():
+    for i in RING_LIST:
+        res = requests.get(f'http://172.17.0.2:26657/abci_query?data="{i}"')
+        # print(res.text)
+        trx_value = json.loads(res.text)["result"]["response"]["value"]
+        b64_value = base64.standard_b64decode(trx_value)
+        b64_list = str(b64_value).split('b"b')[1].replace('"','').replace('\'','')
+        b64_bytes = bytes(b64_list, 'utf-8') 
+        # print(base58.b58decode(b64_bytes))
+        with open(PATH + "xyz_" + i,"wb") as file:
+            file.write(base58.b58decode(b64_bytes))
+    print("DONE!!!")
 
 if __name__ == "__main__":
-    notify(PATH)
+    # notify(PATH)
+    # get_transaction_value("container.builder","x_container.builder")
+    get_trx()
